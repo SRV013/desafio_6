2f8943a95b9255868001b81a2492ff10d1eb8ff9
YAMIPC <97910311+SRV013@users.noreply.github.com>
2023-01-14 15:58:53 -0300

    Update db.ts

--- backend/db.ts
+++ backend/db.ts
@@ -1,5 +1,6 @@
+require('dotenv').config()  
 const admin = require("firebase-admin");
-const serviceAccount = require("./key.json");
+const serviceAccount =process.env.KEY_DB;
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as any),
     databaseURL: "https://desafio-seis-default-rtdb.firebaseio.com"
